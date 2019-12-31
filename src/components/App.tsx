908d2f834256a43495fd4dbf19e89633e2d3528b
Jeff Flower <jeff.flower14@gmail.com>
2019-12-30 20:38:19 -0700

    Add components folder

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <p>Hello world</p>
     </div>
   );
 }
-
-export default App;
