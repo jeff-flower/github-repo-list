856e5286f9231f723a56d325191d54795c4cca7d
Jeff Flower <jeff.flower14@gmail.com>
2020-01-02 21:51:48 -0700

    Refactor tests based on Kent Dodds suggestions

--- src/components/RepoContainer.test.tsx
+++ src/components/RepoContainer.test.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import {render, waitForElement} from '@testing-library/react';
-import axios from 'axios';
 
 import {RepoContainer} from './RepoContainer';
 import {mockRepos} from '../mockRepos';
+import {getTopStarredRepos} from '../githubServices';
 
-// TODO: set up manual mocks if mocking axios gets out control
-jest.mock('axios');
+jest.mock('../githubServices');
 
-it('shows a loading notification', async () => {
-  // @ts-ignore
-  axios.get.mockResolvedValue({data: {items: [], error: false}});
-  const {getByText} = render(<RepoContainer />);
-  await waitForElement(() => getByText('loading', {exact: false}));
+afterEach(() => {
+  jest.resetAllMocks();
 });
 
-it('shows an error notification', async () => {
-  // @ts-ignore
-  axios.get.mockImplementation(() => Promise.reject());
-  const {getByText} = render(<RepoContainer />);
-  await waitForElement(() => getByText('error', {exact: false}));
-});
-
-it('displays the results', async () => {
+test('loads successfully', async () => {
   const mockItems = mockRepos().items;
-  // @ts-ignore
-  axios.get.mockResolvedValue({data: {items: mockItems, error: false}});
-  const {getAllByTestId} = render(<RepoContainer />);
 
+  // @ts-ignore
+  getTopStarredRepos.mockResolvedValue({items: mockItems, error: false});
+
+  const {getByText, getAllByTestId} = render(<RepoContainer />);
+
+  expect(getTopStarredRepos).toHaveBeenCalledTimes(1);
+
+  // loading spinner should show first
+  await getByText('loading', {exact: false});
+
+  // repo cards should show after items load
   const repoCards = await waitForElement(() => getAllByTestId('repoCard'));
   expect(repoCards.length).toBe(mockItems.length);
+});
+
+test('handles an error when fetching data', async () => {
+  // @ts-ignore
+  getTopStarredRepos.mockRejectedValue({items: [], error: true});
+
+  const {getByText} = render(<RepoContainer />);
+
+  expect(getTopStarredRepos).toHaveBeenCalledTimes(1);
+
+  // loading spinner should show first
+  await getByText('loading', {exact: false});
+
+  await waitForElement(() => getByText('error', {exact: false}));
 });
